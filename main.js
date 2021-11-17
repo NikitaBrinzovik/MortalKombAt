2917e0e1cdf10122ca8fb57a721708430edcd0f0
Nikita Kuznetsov <75593249+NikitaBrinzovik@users.noreply.github.com>
2021-11-17 03:40:49 +0300

    init: new button feat: button reload fix: logic for random button

--- main.js
+++ main.js
@@ -7,22 +7,12 @@ const player1 = {
     attack: function () {
         return console.log(`${this.name} Fight...`)
     },
-    fClass: "player1"
-
-
+    elHP: elHP,
+    changeHP: changeHP,
+    renderHP: renderHP,
 }
-const petr = {
-    name: "Petbka",
-    player: 2,
-    hp: 88,
-    img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
-    weapon: ["Vodka", "Seledka"],
-    attack: function () {
-        return console.log(`${this.name} Fight...`)
-    },
-    fClass: "player1"
-}
-const  player2= {
+
+const player2 = {
     player: 2,
     name: "mamitoAleksandrovna",
     hp: 100,
@@ -31,60 +21,38 @@ const  player2= {
     attack: function () {
         return console.log(`${this.name} Fight...`)
     },
-    fClass: "player2"
-
+    elHP: elHP,
+    changeHP: changeHP,
+    renderHP: renderHP,
 }
 
 const arenas = document.querySelector('.arenas')
-const  $randomButton =document.querySelector('.button')
+const $randomButton = document.querySelector('.button')
+const $buttonCreater = document.querySelector('.control')
+arenas.appendChild(createPlayer(player1))
+arenas.appendChild(createPlayer(player2))
 
-function createMyElement(tag, className) {
+function createNewElement(tag, className) {
     const $tag = document.createElement(tag)
-    if(className){
+    if (className) {
         $tag.classList.add(className)
     }
     return $tag
 }
 
-const randomNumber = () => Math.ceil(Math.random()* 20)
-
-function changeHP(player, helth) {
-    player.hp -=randomNumber();
-    if(player.hp > 0) {
-        return helth.style.width = player.hp + '%'
-    }
-    $randomButton.disabled = true
-    winner(player.name=== player1 ? player2.name  : player1.name)
-    return helth.style.width = "0%";
-}
-
-const $winnerTitle = createMyElement('div', "winnerTitle")
-console.log($winnerTitle)
-const winner =(name)=> {
-     arenas.appendChild($winnerTitle)
-    return $winnerTitle.innerText = name + " wins!";
-}
-
 function createPlayer(playerC) {
-//const createPlayer = ( playerC) => {
-
-    const player1 = createMyElement("div", "player"+playerC.player)
-    const progressbar = createMyElement("div", "progressbar")
-    const life = createMyElement("div","life")
-    const name = createMyElement("div", "name")
-    const character = createMyElement("div","character")
-    const img = createMyElement('img', "img")
-
-
-
-    // .appendChild(player1)
+    const player1 = createNewElement("div", "player" + playerC.player)
+    const progressbar = createNewElement("div", "progressbar")
+    const life = createNewElement("div", "life")
+    const name = createNewElement("div", "name")
+    const character = createNewElement("div", "character")
+    const img = createNewElement('img', "img")
 
     progressbar.appendChild(name)
     progressbar.appendChild(life)
     character.appendChild(img)
     player1.appendChild(progressbar)
     player1.appendChild(character)
-
 
     life.style.width = playerC.hp + "%"
     name.innerText = playerC.name
@@ -93,23 +61,78 @@ function createPlayer(playerC) {
     return player1
 }
 
+const getRandomNumber = () => Math.ceil(Math.random() * 20)
 
-console.log(player1)
-console.log(player2)
+function changeHP(hitPoints) {
+    this.hp -= hitPoints
 
+    if (this.hp > 0) {
+        return this.hp
+    }
 
-
-arenas.appendChild(createPlayer(player1))
-arenas.appendChild(createPlayer(player2))
-
-const $playerLife = document.querySelector('.player1 .life')
-const $playerLife2 = document.querySelector('.player2 .life')
-$randomButton.addEventListener('click',  () =>{
-    changeHP(player1, $playerLife)
-    changeHP(player2, $playerLife2)
-})
-
-////////////////helpers
-const onClickF = () => {
-    winner(player1)
+    return this.hp = 0
 }
+
+function elHP() {
+    return document.querySelector(`.player${this.player} .life`)
+}
+
+function renderHP() {
+    this.elHP().style.width = this.hp + '%'
+}
+
+function changeHPPlayers() {
+    player1.changeHP(getRandomNumber())
+    player2.changeHP(getRandomNumber())
+
+    player1.renderHP()
+    player2.renderHP()
+}
+
+function winner(name) {
+    const $winnerTitle = createNewElement('div', 'winnerTitle')
+    if (name) {
+        $winnerTitle.innerText = name + 'wins!'
+        return $winnerTitle
+    }
+    $winnerTitle.innerText = 'draw'
+    return $winnerTitle
+}
+
+function createReloadButton() {
+    const $reloadWrap = createNewElement('div', 'reloadWrap');
+    const $restartButton = createNewElement('button', 'button');
+
+    $restartButton.innerText = 'Restart';
+    $reloadWrap.appendChild($restartButton);
+
+    return $reloadWrap;
+}
+
+function showRestartButton() {
+    $buttonCreater.appendChild(createReloadButton())
+    const $buttonRestart = document.querySelector(`.reloadWrap`, '.button');
+    $buttonRestart.addEventListener('click', function () {
+        window.location.reload()
+    })
+}
+
+
+$randomButton.addEventListener('click', function () {
+    changeHPPlayers()
+
+
+    if (player1.hp === 0 || player2.hp === 0) {
+        $randomButton.disabled = true
+        showRestartButton()
+
+        if (player2.hp === 0 && player1.hp !== 0) {
+            return arenas.appendChild(winner(player1.name))
+        }
+        if (player1.hp === 0 && player2.hp !== 0) {
+            return arenas.appendChild(winner(player2.name))
+        }
+
+        return arenas.appendChild(winner())
+    }
+})
