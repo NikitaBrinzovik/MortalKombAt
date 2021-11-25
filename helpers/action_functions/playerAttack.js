8d2cef9d65e98fc3c76d577d7da550759c5310fd
Nikita Kuznetsov <75593249+NikitaBrinzovik@users.noreply.github.com>
2021-11-25 15:03:38 +0300

    init: chat feat: fix:

--- helpers/action_functions/playerAttack.js
+++ helpers/action_functions/playerAttack.js
@@ -5,20 +5,18 @@ import {HIT} from "../../store/hit_points.js";
 
 export function playerAttack() {
     const attack = {}
-    for (let itemss of $controlForm) {
-        let {checked, name, value} =itemss
+    for (let item of $controlForm) {
+        let {checked, name, value} =item
         if (checked && name === 'hit') {
             attack.value = getRandomNumber(HIT[value])
-            attack.hit = itemss.value
+            attack.hit = value
         }
 
         if (checked && name === 'defence') {
             attack.defence = value
         }
-        /**
-        * Зар привет! Подскажи пожалуйста, почему если я тут пишу просто checked - не срабатывает сброс?
-         **/
-        itemss.checked = false
+
+        item.checked = false
     }
 
     return attack
